36d624bec9aab7a3c4f1e3c9b12c8eb8ba5eb13f
Chris Wage <cwage@quietlife.net>
2016-11-04 18:21:08 -0500

    fixed ownership of .ssh dirs and changed yeargin's username

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -12,6 +12,9 @@ class users {
 
     file { '/home/cwage/.ssh':
         ensure => directory,
+        owner => 'cwage',
+        group => 'cwage',
+        mode => 0700,
         require => User['cwage']
     }
 
@@ -24,23 +27,26 @@ class users {
         require => File['/home/cwage/.ssh']
     }
 
-    user { 'stephenyeargin':
+    user { 'syeargin':
         ensure     => 'present',
-        home       => '/home/stephenyeargin',
+        home       => '/home/syeargin',
         managehome => true,
         comment    => 'Stephen Yeargin',
         groups     => ['www-data', 'sudo' ],
         shell      => '/bin/bash',
     }
 
-    file { '/home/stephenyeargin/.ssh':
+    file { '/home/syeargin/.ssh':
         ensure => directory,
-        require => User['stephenyeargin']
+        owner => 'syeargin',
+        group => 'syeargin',
+        mode => 0700,
+        require => User['syeargin']
     }
 
-    ssh_authorized_key { 'stephenyeargin':
+    ssh_authorized_key { 'syeargin':
         ensure  => present,
-        user    => 'stephenyeargin',
+        user    => 'syeargin',
         type    => 'ssh-rsa',
         key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCZ3NhFpYPm/xey4e0ujjlTOjvSJpNNC7dCR6ncGc7NZKkhKJVcx7H8I1sv4byQRkQ4caM8ehzn8j01KC9me/ZmcWz8MGIFY6lTp5W6r0aG97OVSXK+qyDWkEWEYoUYcWGsmjsfXjBhpWkHTkzYhtvl6VebEBRvXKSnvsln1n7x893w0itzGsT5abvMQS3wTsYVf1bHaWptagXObGaok/cngsNivfmWKp/SeB6/KQg/BAa5vsG5unkOZYtiUYZY4hclzSENl9gYV1xyYJh1b0sa5cCnokahAY+vOfpKMqq0ixfvvT3n+NtiLvfq4fUGgNeRAskv9Cza/f60gwI8g8Ah',
         require => File['/home/cwage/.ssh']
