6399355a184c94477d7ffd5a6ebe7800689affa2
Chris Wage <cwage@quietlife.net>
2016-11-04 15:06:36 -0500

    added puppet module

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,8 +1,26 @@
-class puppet::client {
+class puppet {
     file { '/etc/puppet/puppet.conf':
         owner   => root,
         group   => root,
         mode    => 644,
         source  => 'puppet://modules/puppet/puppet.conf'
     }
+
+    file { 'post-hook':
+        ensure  => file,
+        path    => '/etc/puppet/.git/hooks/post-merge',
+        source  => 'puppet:///modules/puppet/post-merge',
+        mode    => 0755,
+        owner   => root,
+        group   => root,
+    }
+
+    cron { 'puppet-apply':
+        ensure  => present,
+        command => "cd /etc/puppet ; /usr/bin/git pull",
+        user    => root,
+        minute  => '*/30',
+        require => File['post-hook'],
+    }
+
 }
