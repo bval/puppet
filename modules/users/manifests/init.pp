2b0faf64356bc684953b5a753003b2f6c8bb9a1b
Chris Wage <cwage@quietlife.net>
2016-11-04 18:17:59 -0500

    i guess ssh_authorized_key doesn't create the dir

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -10,12 +10,18 @@ class users {
         shell      => '/bin/bash',
     }
 
+    file { '/home/cwage/.ssh':
+        ensure => directory,
+        require => User['cwage']
+    }
+
+
     ssh_authorized_key { 'cwage@quietlife.net':
         ensure  => present,
         user    => 'cwage',
         type    => 'ssh-rsa',
         key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCZ3NhFpYPm/xey4e0ujjlTOjvSJpNNC7dCR6ncGc7NZKkhKJVcx7H8I1sv4byQRkQ4caM8ehzn8j01KC9me/ZmcWz8MGIFY6lTp5W6r0aG97OVSXK+qyDWkEWEYoUYcWGsmjsfXjBhpWkHTkzYhtvl6VebEBRvXKSnvsln1n7x893w0itzGsT5abvMQS3wTsYVf1bHaWptagXObGaok/cngsNivfmWKp/SeB6/KQg/BAa5vsG5unkOZYtiUYZY4hclzSENl9gYV1xyYJh1b0sa5cCnokahAY+vOfpKMqq0ixfvvT3n+NtiLvfq4fUGgNeRAskv9Cza/f60gwI8g8Ah',
-        require => User['cwage']
+        require => File['/home/cwage/.ssh']
     }
 
     user { 'stephenyeargin':
@@ -27,12 +33,17 @@ class users {
         shell      => '/bin/bash',
     }
 
+    file { '/home/stephenyeargin/.ssh':
+        ensure => directory,
+        require => User['stephenyeargin']
+    }
+
     ssh_authorized_key { 'stephenyeargin':
         ensure  => present,
         user    => 'stephenyeargin',
         type    => 'ssh-rsa',
         key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCZ3NhFpYPm/xey4e0ujjlTOjvSJpNNC7dCR6ncGc7NZKkhKJVcx7H8I1sv4byQRkQ4caM8ehzn8j01KC9me/ZmcWz8MGIFY6lTp5W6r0aG97OVSXK+qyDWkEWEYoUYcWGsmjsfXjBhpWkHTkzYhtvl6VebEBRvXKSnvsln1n7x893w0itzGsT5abvMQS3wTsYVf1bHaWptagXObGaok/cngsNivfmWKp/SeB6/KQg/BAa5vsG5unkOZYtiUYZY4hclzSENl9gYV1xyYJh1b0sa5cCnokahAY+vOfpKMqq0ixfvvT3n+NtiLvfq4fUGgNeRAskv9Cza/f60gwI8g8Ah',
-        require => User['stephenyeargin']
+        require => File['/home/cwage/.ssh']
     }
 
 }
