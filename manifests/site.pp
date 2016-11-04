8d68d8b2857b145a016b8a0ce730af33d1fd5528
Chris Wage <cwage@quietlife.net>
2016-11-04 17:40:13 -0500

    fixed indentation

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,10 +6,10 @@ node default {
     include puppet
 
 
-class { '::mysql::server':
-  root_password           => hiera('mysql_root_password'),
-  remove_default_accounts => true,
-}
+    class { '::mysql::server':
+        root_password           => hiera('mysql_root_password'),
+        remove_default_accounts => true,
+    }
 
     # misc packages
     package { [
