889b869882140a6bfda1b17d6a962f62e7c63786
Chris Wage <cwage@quietlife.net>
2016-11-04 18:31:43 -0500

    fixed yeargins' key

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -48,7 +48,7 @@ class users {
         ensure  => present,
         user    => 'syeargin',
         type    => 'ssh-rsa',
-        key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCZ3NhFpYPm/xey4e0ujjlTOjvSJpNNC7dCR6ncGc7NZKkhKJVcx7H8I1sv4byQRkQ4caM8ehzn8j01KC9me/ZmcWz8MGIFY6lTp5W6r0aG97OVSXK+qyDWkEWEYoUYcWGsmjsfXjBhpWkHTkzYhtvl6VebEBRvXKSnvsln1n7x893w0itzGsT5abvMQS3wTsYVf1bHaWptagXObGaok/cngsNivfmWKp/SeB6/KQg/BAa5vsG5unkOZYtiUYZY4hclzSENl9gYV1xyYJh1b0sa5cCnokahAY+vOfpKMqq0ixfvvT3n+NtiLvfq4fUGgNeRAskv9Cza/f60gwI8g8Ah',
+        key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCw2x0TP8ZnAlAmwAKMzuUKFiZK6jCSqr+iMVPDrrjtwWTHxK5K/bePghhsxbO0XRsg781TLUlGqHPktdUilXPvjCSsYnDFB8M3/z39NQWBE9tQA5fSuwLcv1ccWMedVwbVc2Kske3gGGPH2hazcSTq5Hk3ZRmzycZcwzjFbav+eflPE0dccNi/UdDLNwyBO0QTNEM0wppymK6OBF2LcYkefop+uHa1aYj4zn4zGEXH17Is4E/lP3uWRhDvUnM7Ke4mVjvyTIMOq+OYi2hiXBu4N6yTr0ER3HCvgkWUWUXzmRexk1divzAdugDtlvTMh0EJGbRFnP0YF9tunFPyveWV',
         require => File['/home/cwage/.ssh']
     }
 
