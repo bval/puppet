67b5a297b9a90786b9ec180311c2fc3550885be3
Chris Wage <cwage@quietlife.net>
2016-11-04 15:15:05 -0500

    format cleanup

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,23 +1,24 @@
+# Base puppet configuration and cron to run regularly
 class puppet {
     file { '/etc/puppet/puppet.conf':
-        owner   => root,
-        group   => root,
-        mode    => '0644',
-        source  => 'puppet://modules/puppet/puppet.conf'
+        owner  => root,
+        group  => root,
+        mode   => '0644',
+        source => 'puppet:///modules/puppet/puppet.conf'
     }
 
     file { 'post-hook':
-        ensure  => file,
-        path    => '/etc/puppet/.git/hooks/post-merge',
-        source  => 'puppet:///modules/puppet/post-merge',
-        mode    => '0755',
-        owner   => root,
-        group   => root,
+        ensure => file,
+        path   => '/etc/puppet/.git/hooks/post-merge',
+        source => 'puppet:///modules/puppet/post-merge',
+        mode   => '0755',
+        owner  => root,
+        group  => root,
     }
 
     cron { 'puppet-apply':
         ensure  => present,
-        command => "cd /etc/puppet ; /usr/bin/git pull",
+        command => 'cd /etc/puppet ; /usr/bin/git pull',
         user    => root,
         minute  => '*/5',
         require => File['post-hook'],
