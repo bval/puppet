a4517024936416bff03665c63fc2cea9fb2c9146
Chris Wage <cwage@quietlife.net>
2016-11-04 19:04:47 -0500

    added more secure sshd_config

--- modules/hardening/manifests/init.pp
+++ modules/hardening/manifests/init.pp
@@ -18,4 +18,19 @@ class hardening {
         port => '443',
     }
 
+    # More sane SSH config
+    file { '/etc/ssh/sshd_config':
+        ensure => 'present',
+        source => 'puppet:///modules/hardening/sshd_config',
+        notify => Service['ssh'],
+    }
+
+    service { 'ssh':
+        ensure  => 'running',
+        enable  => true,
+        require => Package['openssh-server'],
+    }
+
+
+
 }
