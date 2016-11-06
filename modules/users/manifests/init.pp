f00e5f4c3eea070638d1ccff5cf0b6fd60dba52d
Chris Wage <cwage@quietlife.net>
2016-11-05 21:46:33 -0500

    added beth and fixed some dumb copypasta mistakes

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -56,7 +56,7 @@ class users {
         ensure     => 'present',
         home       => '/home/codeyh',
         managehome => true,
-        comment    => 'Stephen Yeargin',
+        comment    => 'Codey Holland',
         groups     => ['www-data', 'sudo' ],
         shell      => '/bin/bash',
     }
@@ -74,8 +74,32 @@ class users {
         user    => 'codeyh',
         type    => 'ssh-rsa',
         key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQClF53F3WnXEcZ81uBckjCmgm8ozW6NR/ywubtnxTFLV5aR3YMgTzlw2NoLIGU3sJvyygf2rhdiWIE5OzKjDi0OCoaR3CTnAWo3QwzdGXiwuHxiXcH/VW5mfeKEvHL7sQ8SVjh+O1fMa4it2H/+tudLSM//T0qq6tT6qNlxO/Q8vb+KnuK7yjyrywFm+cFWN+O2ZOLFnmHTemzj5WxcqWPFSUaQToK8+zka97isyC2bj+SnfIsFYgxQLwPzknIFl9KwI7BCUK9MlmvaCgHIsuu/1VACVtGcXe7sRxBaACQmOyistFmL0kOMQ0gUOwC/qtZlIYsYEfuukJDIT3OGlgLN',
-        require => File['/home/cwage/.ssh']
+        require => File['/home/codeyh/.ssh']
     }
 
+    user { 'bethdowney':
+        ensure     => 'present',
+        home       => '/home/bethdowney',
+        managehome => true,
+        comment    => 'Beth Downey',
+        groups     => ['www-data', 'sudo' ],
+        shell      => '/bin/bash',
+    }
+
+    file { '/home/bethdowney/.ssh':
+        ensure  => directory,
+        owner   => 'bethdowney',
+        group   => 'bethdowney',
+        mode    => '0700',
+        require => User['bethdowney']
+    }
+
+    ssh_authorized_key { 'bethdowney':
+        ensure  => present,
+        user    => 'bethdowney',
+        type    => 'ssh-rsa',
+        key     => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDjtiqNwhfkpSmwv3epystoX9AgWXNVuxi8MARa5YjKS2FwD5eMMoccR9eMbFRP+Z64gbnbD8/EHqM3UZJKpYamRoXSg3GnlcQbWVM1WpELPn1x+5uj8YWfr5J0fhabRaIgLhaI4pizn5gSXdnjhltv3HNMGUZ1tMZtCzpeDPacAeZjXeRutqI36E3cb2sp6OsIox/Su4K2zcr++kl5YUk+ACA494R7XWQgpV/2Sy4T5Lqf4BBtud1AH1Fzfwj1sZzuwvmLV7pEbb+K3jMfWwsAANj3Zo7WTkDsCE806NQmEJR6nXit7G2rWdnjecD51eSLYBuHYnf2aFisnqlAHDCx7HvMGGy5XXxsJvjNOxghrpYZC2CAwdOAC+0gUztgW1udsNjYMSS/RUwBuI/QVZMtmsDL70x+yw29ApsCkec833RFfHnO1E7iM811wr1iP9htGacIvLefyO1Skk9qvo3ImfaI/lixWis8Qf+iaP2WIqrh35PvygCuT5aR082MQbx1Vv4Zq6SVMbN3xUAqmSHRwaU1ybB67BZE/fKVDpdycbFiGHB4rh/sm+xq/mT8iC8xRspRzF3lDhNLotO+Ku6nBp0vifkHYtFCseqVaLjKmo1KUr9OQbRn1bGJkRoURmOs5zNyu2CtHlrP/Ym8E6K8KIIK7wjslY+wNBxGzikezw==',
+        require => File['/home/bethdowney/.ssh']
+    }
 
 }
